730876c78f976f970c07d9a8c8af11c4322dbd89
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-07-16 23:51:19 -0400

    added controllers to App.js

---
+++ app.js
@@ -0,0 +1,36 @@
+//BUDGET CONTROLLER
+var budgetController = (function(){
+  
+
+})();
+
+// UI CONTROLLER
+var UIController = (function(){
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+  var ctrlAddItem = function () {
+      // 1. Get input data
+      // 2. Add the item to the budget controller
+      // 3. Add the item to the UI
+      // 4. Calculate the Budget
+      // 5. Display the budget on the UI
+      console.log('it works')
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event){
+
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+
+  })
+
+  
+})(budgetController, UIController);
