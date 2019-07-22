0c2b086d8f630775415fb446d5dae19f4d84a9a0
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-07-21 23:58:49 -0400

    added more functions to controllers

--- app.js
+++ app.js
@@ -8,45 +8,65 @@ var budgetController = (function(){
 var UIController = (function(){
 
     var DOMstrings = {
-      inputType: '.add__type',
-      inputDescription: 'add__description',
-      inputValue: '.add__value'
+      inputType: '.add__type', //will be inc or exp
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputBtn: '.add__btn'
     }
 
     return {
       getInput: function() {
-        var type = document.querySelector(DOMstrings.inputType).nodeValue;
-        var description = document.querySelector('.add__description').nodeValue;
-        var value = document.querySelector('.add__value').value;
-      }      
-    }
+        return {
+          type: document.querySelector(DOMstrings.inputType).value,
+          description: document.querySelector(DOMstrings.inputDescription).value,
+          value: document.querySelector(DOMstrings.inputValue).value
+        }; 
+      }, 
+      getDOMSTrings: function() {
+          return DOMstrings;
+      }
 
-    
+    }    
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+  
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMSTrings();
 
-  var ctrlAddItem = function () {
-      // 1. Get input data
-      var input = UICtrl.getInput();
-      // 2. Add the item to the budget controller
-      // 3. Add the item to the UI
-      // 4. Calculate the Budget
-      // 5. Display the budget on the UI
-      console.log('it works')
-  }
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function (event){
+    document.addEventListener('keypress', function (event){
 
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
 
-  })
+    })
+  }
+  
+  
 
+  var ctrlAddItem = function () {
+      // 1. Get input data
+      var input = UICtrl.getInput();
+      
+      // 2. Add the item to the budget controller
+      // 3. Add the item to the UI
+      // 4. Calculate the Budget
+      // 5. Display the budget on the UI
+      
+  }
+
+  return {
+      init: function() {
+        console.log("application has started");
+        setupEventListeners();
+      }
+  }
   
 })(budgetController, UIController);
+
+controller.init();
