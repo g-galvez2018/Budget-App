fb30a97ec9f2bd426a1e86f167c799e3f0bb98b7
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-07-28 23:54:46 -0400

    adding new Item data in UI Controller

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ var UIController = (function(){
       inputType: '.add__type', //will be inc or exp
       inputDescription: '.add__description',
       inputValue: '.add__value',
-      inputBtn: '.add__btn'
+      inputBtn: '.add__btn',
+      incomeContainer: '.income__list',
+      expensesContainer: '.expenses__list'
     }
 
     return {
@@ -85,10 +87,12 @@ var UIController = (function(){
           var html;
          // Create HTML string with placeholder text
          if (type === 'inc'){
+          element = DOMstrings.incomeContainer
           html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div> </div>';
          }
 
          else if (type === 'exp'){
+           element = DOMstrings.incomeContainer
            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div> <div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
          }
 
@@ -98,16 +102,8 @@ var UIController = (function(){
         newHtml = newHtml.replace('%value%', obj.value);
 
          // Insert the HTML into the DOM
-      },
-
-      addListItem: function (obj, type) {
-
-          // Create HMTL string with placeholder text
-
-          // Replace the placehoolder text with some actual data
-
-          // Insert the HTML into the DOM
-      },
+         document.querySelector(element).insertAdjacentHtml('beforeend', newHtml);
+      },     
 
       getDOMSTrings: function() {
           return DOMstrings;
